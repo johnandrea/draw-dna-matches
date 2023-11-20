f0f4b74b077ffbb9c8e06e26819c2900e3af1aa2
johnandrea <46268748+johnandrea@users.noreply.github.com>
2023-11-20 12:46:00 -0400

    Add examples for format=matrix

--- test-data/run-larger.cmd
+++ test-data/run-larger.cmd
@@ -5,9 +5,12 @@ if exist run.err del run.err
 if exist larger.dot del larger.dot
 if exist larger.png del larger.png
 if exist larger.svg del larger.svg
+if exist larger-matrix.dot del larger-matrix.dot
+if exist larger-matrix.png del larger-matrix.png
+if exist larger-matrix.svg del larger-matrix.svg
 
 draw-dna-matches.py --relation dnamatch larger.ged >larger.dot 2>run.err
-draw-dna-matches.py --format-matrix --relation dnamatch larger.ged >larger-matrix.dot 2>run.err
+draw-dna-matches.py --format=matrix --relation dnamatch larger.ged >larger-matrix.dot 2>run.err
 
 set graphviz="c:\Program files\Graphviz\bin\dot.exe"
 
--- test-data/run-test-family.cmd
+++ test-data/run-test-family.cmd
@@ -5,8 +5,12 @@ del run.err
 del test-family.dot
 del test-family.png
 del test-family.svg
+del test-family-matrix.dot
+del test-family-matrix.png
+del test-family-matrix.svg
 
 draw-dna-matches.py --relation dnamatch test-family.ged >test-family.dot 2>run.err
+draw-dna-matches.py --format=matrix --relation dnamatch test-family.ged >test-family-matrix.dot 2>>run.err
 
 set graphviz="c:\Program files\Graphviz\bin\dot.exe"
 
@@ -14,6 +18,8 @@ if exist %graphviz% (
 
    %graphviz% -Tpng test-family.dot -o test-family.png >run.out 2>>run.err
    %graphviz% -Tsvg test-family.dot -o test-family.svg
+   %graphviz% -Tpng test-family-matrix.dot -o test-family-matrix.png >>run.out 2>>run.err
+   %graphviz% -Tsvg test-family-matrix.dot -o test-family-matrix.svg
 
 ) else (
   echo graphviz missing
